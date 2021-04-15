6c05a6e2d50794931dc17801d45e90f5ba29a80a
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-04-16 09:14:52 +1000

    Started implementing validator.

--- Sources/VHDLMachines/Clock.swift
+++ Sources/VHDLMachines/Clock.swift
@@ -9,8 +9,23 @@ import Foundation
 
 public struct Clock {
     
+    public enum FrequencyUnit: String, CaseIterable {
+        
+        case Hz = "Hz"
+        
+        case kHz = "kHz"
+        
+        case MHz = "MHz"
+        
+        case GHz = "GHz"
+        
+        case THz = "THz"
+    }
+    
     public var name: String
     
     public var frequency: UInt
+    
+    public var unit: FrequencyUnit
     
 }
