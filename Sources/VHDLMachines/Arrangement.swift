ce39095c0881b3abe9a5b6cc7b819078df264676
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-04-16 11:44:05 +1000

    Finished Arrangement.

--- Sources/VHDLMachines/Arrangement.swift
+++ Sources/VHDLMachines/Arrangement.swift
@@ -7,14 +7,27 @@
 
 import Foundation
 
-struct Arrangement {
+public struct Arrangement {
     
-    var machines: [URL]
+    public var machines: [MachineName: URL]
     
-    var externalSignals: [ExternalSignal]
+    public var externalSignals: [ExternalSignal]
     
-    var externalVariables: [VHDLVariable]
+    public var externalVariables: [VHDLVariable]
     
-    var clocks: [Clock]
+    public var clocks: [Clock]
+    
+    public var parents: [MachineName] // Parent Machines
+    
+    public var path: URL
+    
+    public init(machines: [MachineName: URL], externalSignals: [ExternalSignal], externalVariables: [VHDLVariable], clocks: [Clock], parents: [MachineName], path: URL) {
+        self.machines = machines
+        self.externalSignals = externalSignals
+        self.externalVariables = externalVariables
+        self.clocks = clocks
+        self.parents = parents
+        self.path = path
+    }
     
 }
