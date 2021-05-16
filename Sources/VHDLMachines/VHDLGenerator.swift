422811a20fee3ad7cd6d23e715c446b556be45a6
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-05-17 00:05:07 +1000

    Created dummy generator.

---
+++ Sources/VHDLMachines/VHDLGenerator.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Morgan McColl on 17/5/21.
+//
+
+public struct VHDLGenerator {
+    
+    public init() {}
+    
+    public func generate(machine: Machine) -> Bool {
+        print("Generation not yet implemented!")
+        return true
+    }
+    
+}
