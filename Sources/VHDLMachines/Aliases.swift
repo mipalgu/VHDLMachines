fc38e295dbd9e3b4164159195465fecf3dc5b81b
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-14 22:18:34 +1000

    Commented aliases.

--- Sources/VHDLMachines/Aliases.swift
+++ Sources/VHDLMachines/Aliases.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 
+/// The type of a machine's name.
 public typealias MachineName = String
 
+/// The type of a machine's action.
 public typealias ActionName = String
 
-//public typealias SignalType = String
+// public typealias SignalType = String
 //
-//public typealias VariableType = String
+// public typealias VariableType = String
 
+/// The type of a machine's parameter type.
 public typealias ParameterType = String
