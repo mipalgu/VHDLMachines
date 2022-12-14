a70a778d7024e9c4fb82ca5d36b7f813148d111b
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-14 23:55:26 +1000

    Tested VHDLVariable.

--- Sources/VHDLMachines/VHDLVariable.swift
+++ Sources/VHDLMachines/VHDLVariable.swift
@@ -7,18 +7,32 @@
 
 import Foundation
 
+/// A variable that is local to an architecture implementation of a VHDL entity. This variable
+/// is equivalent to a LLFSM machine variable or State variable.
 public struct VHDLVariable: Variable {
-    
+
+    /// The type of the variable.
     public var type: String
-    
+
+    /// The name of the variable.
     public var name: String
-    
+
+    /// The default value of the variable.
     public var defaultValue: String?
-    
+
+    /// The range of the variable.
     public var range: (Int, Int)?
-    
+
+    /// The comment of the variable.
     public var comment: String?
-    
+
+    /// Initialises a ``VHDLVariable`` with a type, name, default value, range and comment.
+    /// - Parameters:
+    ///   - type: The type of the variable.
+    ///   - name: The name of the variable.
+    ///   - defaultValue: The default value of the variable.
+    ///   - range: The range of valid values for the variable.
+    ///   - comment: The comment of the variable.
     public init(type: String, name: String, defaultValue: String?, range: (Int, Int)?, comment: String?) {
         self.type = type
         self.name = name
@@ -26,15 +40,21 @@ public struct VHDLVariable: Variable {
         self.range = range
         self.comment = comment
     }
-    
+
 }
 
+/// Conformance to ``Codable``.
 extension VHDLVariable: Codable {
-    
+
+    /// The keys used to encode and decode the variable.
     enum CodingKeys: CodingKey {
+
         case type, name, defaultValue, range, comment
+
     }
-    
+
+    /// Initialises a ``VHDLVariable`` from a decoder.
+    /// - Parameter from: The decoder to decode the variable from.
     public init(from: Decoder) throws {
         let container = try from.container(keyedBy: CodingKeys.self)
         type = try container.decode(String.self, forKey: .type)
@@ -56,7 +76,9 @@ extension VHDLVariable: Codable {
         }
         range = (minRange, maxRange)
     }
-    
+
+    /// Encodes the variable.
+    /// - Parameter encoder: The encoder to encode the variable to. 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(type, forKey: .type)
@@ -64,10 +86,10 @@ extension VHDLVariable: Codable {
         try container.encode(defaultValue, forKey: .defaultValue)
         try container.encode(comment, forKey: .comment)
         guard let range = range else {
-            try container.encode(Optional<String>(nil), forKey: .range)
+            try container.encode(String?(nil), forKey: .range)
             return
         }
-        try container.encode(Optional<String>("\(range.0),\(range.1)"), forKey: .range)
+        try container.encode(String?("\(range.0),\(range.1)"), forKey: .range)
     }
-    
+
 }
