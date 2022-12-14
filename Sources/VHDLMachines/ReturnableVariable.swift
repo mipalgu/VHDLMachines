673c56c303182f377cf58ad9866aee01098fb11f
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-14 23:44:09 +1000

    Tested ReturnableVariable.

--- Sources/VHDLMachines/ReturnableVariable.swift
+++ Sources/VHDLMachines/ReturnableVariable.swift
@@ -7,22 +7,33 @@
 
 import Foundation
 
+/// A variable that can be returned from a parameterised machine.
 public struct ReturnableVariable: ExternalType, Codable {
-    
-    public var defaultValue: String? = nil
-    
+
+    /// The default value is nil for a returnable variable.
+    public var defaultValue: String?
+
+    /// The mode is an output for a returnable variable.
     public var mode: Mode = .output
-    
+
+    /// The type of the variable.
     public var type: ParameterType
-    
+
+    /// The name of the variable.
     public var name: String
-    
+
+    /// The comment for the variable.
     public var comment: String?
-    
+
+    /// Initialises a returnable variable with the given type, name and comment.
+    /// - Parameters:
+    ///   - type: The type of the variable.
+    ///   - name: The name of the variable.
+    ///   - comment: The comment for the variable.
     public init(type: ParameterType, name: String, comment: String? = nil) {
         self.type = type
         self.name = name
         self.comment = comment
     }
-    
+
 }
