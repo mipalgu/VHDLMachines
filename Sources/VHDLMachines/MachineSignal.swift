8984e3e9dca8136f6533f2769c6a2a8f8eedd316
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-14 22:38:01 +1000

    Tested MachineSignal.

--- Sources/VHDLMachines/MachineSignal.swift
+++ Sources/VHDLMachines/MachineSignal.swift
@@ -7,16 +7,28 @@
 
 import Foundation
 
+/// A machine signal is a signal that exists within the scope of a VHDL entity. It is a signal that is defined
+/// within a machine and can be though of as a type of machine variable in VHDL.
 public struct MachineSignal: Variable, Codable {
-    
+
+    /// The type of the signal.
     public var type: String
-    
+
+    /// The name of the signal.
     public var name: String
-    
+
+    /// The default value of the signal.
     public var defaultValue: String?
-    
+
+    /// The comment of the signal.
     public var comment: String?
-    
+
+    /// Initialises a new machine signal with the given type, name, default value and comment.
+    /// - Parameters:
+    ///   - type: The type of the signal.
+    ///   - name: The name of the signal.
+    ///   - defaultValue: The default value of the signal.
+    ///   - comment: The comment of the signal.
     public init(type: String, name: String, defaultValue: String?, comment: String?) {
         self.type = type
         self.name = name
