5ebe37d9333da1d16051a85ca5daee85b0b0a412
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-01-13 11:43:15 +1000

    Separated methods out into separate files.

--- Sources/VHDLMachines/LocalSignal.swift
+++ Sources/VHDLMachines/LocalSignal.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// A local signal is a signal that exists within the scope of a VHDL entity. It is a signal that is defined
 /// within a machine/arrangement and can be though of as a type of machine variable in VHDL.
-public struct LocalSignal: Codable, Equatable, Hashable, Variable, Sendable {
+public struct LocalSignal: RawRepresentable, Codable, Equatable, Hashable, Variable, Sendable {
 
     /// The type of the signal.
     public var type: SignalType
@@ -23,6 +23,14 @@ public struct LocalSignal: Codable, Equatable, Hashable, Variable, Sendable {
     /// The comment of the signal.
     public var comment: String?
 
+    @inlinable public var rawValue: String {
+        let declaration = "signal \(name): \(type.rawValue)"
+        guard let defaultValue = defaultValue else {
+            return declaration + ";"
+        }
+        return declaration + " := \(defaultValue.rawValue);"
+    }
+
     /// Initialises a new machine signal with the given type, name, default value and comment.
     /// - Parameters:
     ///   - type: The type of the signal.
@@ -35,6 +43,49 @@ public struct LocalSignal: Codable, Equatable, Hashable, Variable, Sendable {
         self.name = name
         self.defaultValue = defaultValue
         self.comment = comment
+    }
+
+    public init?(rawValue: String) {
+        let trimmedString = rawValue.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard trimmedString.count < 256, trimmedString.hasPrefix("signal ") else {
+            return nil
+        }
+        let components = trimmedString.components(separatedBy: ";")
+        guard components.count <= 2, !components.isEmpty else {
+            return nil
+        }
+        let comment = String(comment: components.last ?? "")
+        let declaration = trimmedString.uptoSemicolon
+        guard !declaration.contains(":=") else {
+            let declComponents = declaration.components(separatedBy: ":=")
+            guard declComponents.count == 2 else {
+                return nil
+            }
+            self.init(declaration: declComponents[0], defaultValue: declComponents[1], comment: comment)
+            return
+        }
+        self.init(declaration: declaration, comment: comment)
+    }
+
+    private init?(declaration: String, defaultValue: String? = nil, comment: String? = nil) {
+        let signalComponents = declaration.components(separatedBy: .whitespacesAndNewlines)
+        let value = SignalLiteral(rawValue: defaultValue ?? "")
+        guard
+            signalComponents.first == "signal",
+            signalComponents.count == 3,
+            signalComponents[1].hasSuffix(":"),
+            let type = SignalType(rawValue: signalComponents[2])
+        else {
+            return nil
+        }
+        let name = String(signalComponents[1].dropLast())
+        guard !name.isEmpty, !CharacterSet.whitespacesAndNewlines.within(string: name) else {
+            return nil
+        }
+        self.name = name
+        self.type = type
+        self.comment = comment
+        self.defaultValue = value
     }
 
 }
