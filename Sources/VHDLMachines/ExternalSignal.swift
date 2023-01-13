e9d8c2a337013d94b6da6724f8c64e3291d39b83
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-01-13 14:27:28 +1000

    Created external expression.

--- Sources/VHDLMachines/ExternalSignal.swift
+++ Sources/VHDLMachines/ExternalSignal.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// An external signal is equivalent to an external variable (or parameter) in an LLFSM. The external signal
 /// is a signal that exists above a VHDL entities scope. It is a signal that is not defined within the entity.
-public struct ExternalSignal: ExternalType, Codable, Hashable, Variable, Sendable {
+public struct ExternalSignal: ExternalType, RawRepresentable, Codable, Hashable, Variable, Sendable {
 
     /// The type of the signal.
     public var type: SignalType
@@ -26,6 +26,15 @@ public struct ExternalSignal: ExternalType, Codable, Hashable, Variable, Sendabl
     /// The mode of the signal.
     public var mode: Mode
 
+    public var rawValue: String {
+        let declaration = "\(name): \(mode.rawValue) \(type.rawValue)"
+        let comment = self.comment.map { " -- \($0)" } ?? ""
+        guard let defaultValue = defaultValue else {
+            return declaration + ";\(comment)"
+        }
+        return declaration + " := \(defaultValue.rawValue);\(comment)"
+    }
+
     /// Initialises a new external signal with the given type, name, mode, default value and comment.
     /// - Parameters:
     ///   - type: The type of the signal.
@@ -39,6 +48,44 @@ public struct ExternalSignal: ExternalType, Codable, Hashable, Variable, Sendabl
     ) {
         self.type = type
         self.name = name
+        self.mode = mode
+        self.defaultValue = defaultValue
+        self.comment = comment
+    }
+
+    public init?(rawValue: String) {
+        let trimmedString = rawValue.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard trimmedString.count < 256 else {
+            return nil
+        }
+        let components = trimmedString.components(separatedBy: ";")
+        guard components.count <= 2, !components.isEmpty else {
+            return nil
+        }
+        let comment = components.count == 2 ? String(comment: components.last ?? "") : nil
+        let declaration = trimmedString.uptoSemicolon
+        let assignmentComponents = declaration.components(separatedBy: ":=")
+        guard assignmentComponents.count <= 2, let typeDeclaration = assignmentComponents.first else {
+            return nil
+        }
+        let typeComponents = typeDeclaration.components(separatedBy: .whitespaces)
+        guard typeComponents.count >= 3 else {
+            return nil
+        }
+        let nameComponents = typeComponents[0]
+        let modeComponents = typeComponents[1]
+        let typeString = typeComponents[2...].joined(separator: " ")
+        guard let mode = Mode(rawValue: modeComponents), nameComponents.hasSuffix(":") else {
+            return nil
+        }
+        let nameString = String(nameComponents.dropLast())
+        guard let name = String(name: nameString), let type = SignalType(rawValue: typeString) else {
+            return nil
+        }
+        let defaultValue = assignmentComponents.count == 2 ? SignalLiteral(rawValue: assignmentComponents[1])
+            : nil
+        self.name = name
+        self.type = type
         self.mode = mode
         self.defaultValue = defaultValue
         self.comment = comment
