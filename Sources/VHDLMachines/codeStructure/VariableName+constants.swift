2a3f3a22f10b4b05d482458b9fc03f8463e3fc5f
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-02-02 14:39:24 +1000

    Removed zero.

--- Sources/VHDLMachines/codeStructure/VariableName+constants.swift
+++ Sources/VHDLMachines/codeStructure/VariableName+constants.swift
@@ -127,8 +127,6 @@ public extension VariableName {
 
     static var suspendCommand: VariableName { VariableName(rawValue: "COMMAND_SUSPEND")! }
 
-    static var zero: VariableName { VariableName(rawValue: "ZERO")! }
-
     static func name(for state: State) -> VariableName {
         VariableName(rawValue: "STATE_\(state.name.rawValue)")!
     }
