5fbf6203a07a1410070fd9e8975cd22ad73f5bfe
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-02-06 16:05:30 +1000

    Added helper comments and implemented onEntry init.

--- Sources/VHDLMachines/codeStructure/WhenCase+machineInit.swift
+++ Sources/VHDLMachines/codeStructure/WhenCase+machineInit.swift
@@ -62,17 +62,67 @@ extension WhenCase {
         switch action {
         case .readSnapshot:
             self.init(readSnapshotMachine: machine)
+        case .onEntry:
+            self.init(onEntryMachine: machine)
+        case .onExit:
+            self.init(normalAction: action, machine: machine, nextAction: .writeSnapshot)
         default:
             return nil
         }
     }
 
-    // private init?(state: State, action: VariableName, nextAction: VariableName) {
-    //     guard let code = state.actions[action] else {
-    //         return nil
-    //     }
-    //     return WhenC
-    // }
+    private init?(state: State, action: VariableName, nextAction: VariableName) {
+        guard let code = state.actions[action] else {
+            return nil
+        }
+        self.init(condition: .expression(expression: .variable(name: .name(for: state))), code: code)
+    }
+
+    private init(onEntryMachine machine: Machine) {
+        let stateCases = machine.states.compactMap {
+            guard machine.hasAfter(state: $0) else {
+                return WhenCase(state: $0, action: .onEntry, nextAction: .checkTransition)
+            }
+            let condition = WhenCondition.expression(expression: .variable(name: .name(for: $0)))
+            let trailer = SynchronousBlock.statement(statement: .assignment(
+                name: .ringletCounter, value: .literal(value: .integer(value: 0))
+            ))
+            guard let code = $0.actions[.onEntry] else {
+                return WhenCase(condition: condition, code: trailer)
+            }
+            return WhenCase(condition: condition, code: .blocks(blocks: [code, trailer]))
+        }
+        let condition = WhenCondition.expression(expression: .variable(name: .onEntry))
+        let trailer = SynchronousBlock.statement(statement: .assignment(
+            name: .internalState, value: .variable(name: .checkTransition)
+        ))
+        guard !stateCases.isEmpty else {
+            self.init(condition: condition, code: trailer)
+            return
+        }
+        let statement = CaseStatement(
+            condition: .variable(name: .currentState), cases: stateCases + [WhenCase.othersNull]
+        )
+        self.init(condition: condition, code: .blocks(blocks: [.caseStatement(block: statement), trailer]))
+    }
+
+    private init(normalAction: VariableName, machine: Machine, nextAction: VariableName) {
+        let stateCases = machine.states.compactMap {
+            WhenCase(state: $0, action: normalAction, nextAction: nextAction)
+        }
+        let condition = WhenCondition.expression(expression: .variable(name: normalAction))
+        let trailer = SynchronousBlock.statement(statement: .assignment(
+            name: .internalState, value: .variable(name: nextAction)
+        ))
+        guard !stateCases.isEmpty else {
+            self.init(condition: condition, code: trailer)
+            return
+        }
+        let statement = CaseStatement(
+            condition: .variable(name: .currentState), cases: stateCases + [WhenCase.othersNull]
+        )
+        self.init(condition: condition, code: .blocks(blocks: [.caseStatement(block: statement), trailer]))
+    }
 
     // swiftlint:disable function_body_length
 
@@ -89,7 +139,7 @@ extension WhenCase {
                 statement: .assignment(name: $0.name, value: .variable(name: .name(for: $0)))
             )
         }
-        var blocks: [SynchronousBlock] = snapshots
+        // OnEntry semantics. Only perform OnEntry when previousRinglet != currentState.
         let onEntryBlock = IfBlock.ifElse(
             condition: .conditional(condition: .comparison(
                 value: .notEquals(lhs: .variable(name: .previousRinglet), rhs: .variable(name: .currentState))
@@ -107,11 +157,14 @@ extension WhenCase {
             suspendedIndex < machine.states.count
         else {
             self.init(
-                condition: whenCondition, code: .blocks(blocks: blocks + [.ifStatement(block: onEntryBlock)])
+                condition: whenCondition,
+                code: .blocks(blocks: snapshots + [.ifStatement(block: onEntryBlock)])
             )
             return
         }
+        var blocks: [SynchronousBlock] = snapshots
         let suspendedState = machine.states[suspendedIndex]
+        /// Parameterised snapshot semantics.
         if machine.isParameterised {
             let parameterSnapshots = machine.parameterSignals.map {
                 SynchronousBlock.statement(
@@ -119,6 +172,7 @@ extension WhenCase {
                 )
             }
             blocks.append(SynchronousBlock.ifStatement(block: .ifStatement(
+                // Perform snapshot of parameters on restart command.
                 condition: .conditional(condition: .comparison(value: .equality(
                     lhs: .variable(name: .command),
                     rhs: .variable(name: .restartCommand)
@@ -126,9 +180,11 @@ extension WhenCase {
                 ifBlock: .blocks(blocks: parameterSnapshots)
             )))
         }
+        // Suspension semantics.
         blocks += [
             .ifStatement(
                 block: .ifElse(
+                    // Restart semantics.
                     condition: .logical(operation: .and(
                         lhs: .precedence(value: .conditional(condition: .comparison(value: .equality(
                             lhs: .variable(name: .command), rhs: .variable(name: .restartCommand)
@@ -139,6 +195,7 @@ extension WhenCase {
                         ))))
                     )),
                     ifBlock: .blocks(blocks: [
+                        // Set current state to the initial state.
                         .statement(statement: .assignment(
                             name: .currentState, value: .variable(name: .name(for: initialState))
                         )),
@@ -151,6 +208,7 @@ extension WhenCase {
                         .statement(statement: .assignment(
                             name: .targetState, value: .variable(name: .name(for: initialState))
                         )),
+                        // If suspended perform OnResume, else perform OnEntry.
                         .ifStatement(block: .ifElse(
                             condition: .conditional(condition: .comparison(value: .equality(
                                 lhs: .variable(name: .previousRinglet),
@@ -174,6 +232,7 @@ extension WhenCase {
                         ))
                     ]),
                     elseBlock: .ifStatement(block: .ifElse(
+                        // Resume semantics.
                         condition: .logical(operation: .and(
                             lhs: .precedence(value: .conditional(condition: .comparison(value: .equality(
                                 lhs: .variable(name: .command), rhs: .variable(name: .resumeCommand)
@@ -189,6 +248,7 @@ extension WhenCase {
                                 ))))
                             )))
                         )),
+                        // Set current state to the state the machine was suspended from.
                         ifBlock: .blocks(blocks: [
                             .statement(statement: .assignment(
                                 name: .suspended, value: .literal(value: .bit(value: .low))
@@ -199,6 +259,7 @@ extension WhenCase {
                             .statement(statement: .assignment(
                                 name: .targetState, value: .variable(name: .suspendedFrom)
                             )),
+                            // If previous state was suspended perform OnResume, else perform NoOnEntry.
                             .ifStatement(block: .ifElse(
                                 condition: .conditional(condition: .comparison(value: .equality(
                                     lhs: .variable(name: .previousRinglet),
@@ -213,6 +274,7 @@ extension WhenCase {
                             ))
                         ]),
                         elseBlock: .ifStatement(block: .ifElse(
+                            // Suspend semantics.
                             condition: .logical(operation: .and(
                                 lhs: .precedence(value: .conditional(condition: .comparison(value: .equality(
                                     lhs: .variable(name: .command), rhs: .variable(name: .suspendCommand)
@@ -223,6 +285,7 @@ extension WhenCase {
                                 ))))
                             )),
                             ifBlock: .blocks(blocks: [
+                                // Set current state to the suspended state.
                                 .statement(statement: .assignment(
                                     name: .suspendedFrom, value: .variable(name: .currentState)
                                 )),
@@ -235,6 +298,7 @@ extension WhenCase {
                                 .statement(statement: .assignment(
                                     name: .targetState, value: .variable(name: .name(for: suspendedState))
                                 )),
+                                // Perform OnSuspend if not in the suspended state previously.
                                 .ifStatement(block: .ifElse(
                                     condition: .conditional(condition: .comparison(value: .equality(
                                         lhs: .variable(name: .previousRinglet),
@@ -248,15 +312,18 @@ extension WhenCase {
                                     ))
                                 ))
                             ]),
+                            // Suspended state execution.
                             elseBlock: .ifStatement(block: .ifElse(
                                 condition: .conditional(condition: .comparison(value: .equality(
                                     lhs: .variable(name: .currentState),
                                     rhs: .variable(name: .name(for: suspendedState))
                                 ))),
+                                // Set suspended flag to logic high.
                                 ifBlock: .blocks(blocks: [
                                     .statement(statement: .assignment(
                                         name: .suspended, value: .literal(value: .bit(value: .high))
                                     )),
+                                    // Execute OnSuspend if not in the suspended state previously.
                                     .ifStatement(block: .ifElse(
                                         condition: .conditional(condition: .comparison(value: .notEquals(
                                             lhs: .variable(name: .previousRinglet),
@@ -270,11 +337,13 @@ extension WhenCase {
                                         ))
                                     ))
                                 ]),
+                                // Transition from Suspended.
                                 elseBlock: .ifStatement(block: .ifElse(
                                     condition: .conditional(condition: .comparison(value: .equality(
                                         lhs: .variable(name: .previousRinglet),
                                         rhs: .variable(name: .name(for: suspendedState))
                                     ))),
+                                    // Execute onResume.
                                     ifBlock: .blocks(blocks: [
                                         .statement(statement: .assignment(
                                             name: .internalState, value: .variable(name: .onResume)
@@ -286,6 +355,7 @@ extension WhenCase {
                                             name: .suspendedFrom, value: .variable(name: .currentState)
                                         ))
                                     ]),
+                                    // Normal execution.
                                     elseBlock: .blocks(blocks: [
                                         .statement(statement: .assignment(
                                             name: .suspended, value: .literal(value: .bit(value: .low))
@@ -293,6 +363,7 @@ extension WhenCase {
                                         .statement(statement: .assignment(
                                             name: .suspendedFrom, value: .variable(name: .currentState)
                                         )),
+                                        // Decide whether to perform OnEntry.
                                         .ifStatement(block: onEntryBlock)
                                     ])
                                 ))
@@ -306,5 +377,22 @@ extension WhenCase {
     }
 
     // swiftlint:enable function_body_length
+
+}
+
+/// Add `hasAfter` function.
+private extension Machine {
+
+    /// Check whether a state contains a transition with an after condition.
+    /// - Parameter state: The state to check.
+    /// - Returns: Whether this state contains a transition with an after condition.
+    func hasAfter(state: State) -> Bool {
+        guard let index = self.states.firstIndex(of: state) else {
+            return false
+        }
+        return self.transitions.lazy
+            .filter { $0.source == index }
+            .contains { $0.condition.hasAfter }
+    }
 
 }
