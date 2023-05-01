985878240e07529e111ef27dddb98af3693112ab
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-05-01 21:59:53 +1000

    Changed package urls to use https for public repos.

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-        .package(url: "git@github.com:mipalgu/VHDLParsing", from: "1.0.1"),
-        .package(url: "git@github.com:mipalgu/GUUnits", from: "2.1.0"),
-        .package(url: "git@github.com:mipalgu/swift_helpers", from: "2.0.0")
+        .package(url: "https://github.com/mipalgu/VHDLParsing", from: "1.0.1"),
+        .package(url: "https://github.com/mipalgu/GUUnits", from: "2.1.0"),
+        .package(url: "https://github.com/mipalgu/swift_helpers", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
