9a26343622169b9760afa86f40484697f2678ba2
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-06-29 03:53:28 +1000

    Added LLFSMModel as dependency.

--- Package.swift
+++ Package.swift
@@ -3,10 +3,12 @@
 
 import PackageDescription
 
+/// The package definition.
 let package = Package(
     name: "VHDLMachines",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries produced by a package, and make them visible to other
+        // packages.
         .library(name: "VHDLMachines", targets: ["VHDLMachines"])
     ],
     dependencies: [
@@ -15,18 +17,33 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
         .package(url: "https://github.com/mipalgu/VHDLParsing", from: "1.0.1"),
         .package(url: "https://github.com/mipalgu/GUUnits", from: "2.1.0"),
-        .package(url: "https://github.com/mipalgu/swift_helpers", from: "2.0.0")
+        .package(url: "https://github.com/mipalgu/swift_helpers", from: "2.0.0"),
+        .package(url: "https://github.com/mipalgu/LLFSMModel", branch: "model")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages which this package
+        // depends on.
         .target(
             name: "VHDLMachines",
-            dependencies: [.product(name: "swift_helpers", package: "swift_helpers"), .product(name: "IO", package: "swift_helpers"), .product(name: "GUUnits", package: "GUUnits"), .product(name: "VHDLParsing", package: "VHDLParsing"), .product(name: "StringHelpers", package: "VHDLParsing")]
+            dependencies: [
+                .product(name: "swift_helpers", package: "swift_helpers"),
+                .product(name: "IO", package: "swift_helpers"),
+                .product(name: "GUUnits", package: "GUUnits"),
+                .product(name: "VHDLParsing", package: "VHDLParsing"),
+                .product(name: "StringHelpers", package: "VHDLParsing")
+            ]
         ),
         .testTarget(
             name: "VHDLMachinesTests",
-            dependencies: ["VHDLMachines", .product(name: "swift_helpers", package: "swift_helpers"), .product(name: "IO", package: "swift_helpers"), .product(name: "Functional", package: "swift_helpers"), .product(name: "GUUnits", package: "GUUnits"), "VHDLParsing"]
-        ),
+            dependencies: [
+                "VHDLMachines",
+                .product(name: "swift_helpers", package: "swift_helpers"),
+                .product(name: "IO", package: "swift_helpers"),
+                .product(name: "Functional", package: "swift_helpers"),
+                .product(name: "GUUnits", package: "GUUnits"),
+                "VHDLParsing"
+            ]
+        )
     ]
 )
