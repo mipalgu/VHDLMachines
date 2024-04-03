66e4da1555b26c3e885a7acfd6b0486d62ced379
Morgan McColl <morgan.mccoll92@gmail.com>
2024-04-03 22:52:17 +1000

    Removed docc plugin from windows builds.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,25 @@
 
 import PackageDescription
 
+#if !os(Windows)
+/// The package dependencies.
+let dependencies: [PackageDescription.Package.Dependency] = [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+    .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.1"),
+    .package(url: "https://github.com/mipalgu/GUUnits", from: "2.1.0"),
+    .package(url: "https://github.com/mipalgu/LLFSMModel", from: "1.0.0"),
+    .package(url: "https://github.com/cpslabgu/SwiftUtils", from: "0.1.0")
+]
+#else
+/// The package dependencies.
+let dependencies: [PackageDescription.Package.Dependency] = [
+    .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.1"),
+    .package(url: "https://github.com/mipalgu/GUUnits", from: "2.1.0"),
+    .package(url: "https://github.com/mipalgu/LLFSMModel", from: "1.0.0"),
+    .package(url: "https://github.com/cpslabgu/SwiftUtils", from: "0.1.0")
+]
+#endif
+
 /// The package definition.
 let package = Package(
     name: "VHDLMachines",
@@ -11,15 +30,7 @@ let package = Package(
         // packages.
         .library(name: "VHDLMachines", targets: ["VHDLMachines", "ModelImports"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-        .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.1"),
-        .package(url: "https://github.com/mipalgu/GUUnits", from: "2.1.0"),
-        .package(url: "https://github.com/mipalgu/LLFSMModel", from: "1.0.0"),
-        .package(url: "https://github.com/cpslabgu/SwiftUtils", from: "0.1.0")
-    ],
+    dependencies: dependencies,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package
